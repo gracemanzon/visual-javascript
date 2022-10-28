b784a5045790f34a607502c5399de90fa8fbc45b
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-10-28 18:52:13 -0400

    make image dynamic based on canvas dimensions

--- sketch_01.js
+++ sketch_01.js
@@ -1,31 +1,37 @@
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [600, 600],
+  dimensions: [1080, 1080],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.1;
+    const h = height * 0.1;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const offSet = width * 0.02;
+
     let x, y;
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
 
         context.beginPath();
         context.rect(x, y, w, h);
         context.stroke();
 
-        if (Math.random() * 0.5) {
+        if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + 8, y + 8, w - 16, h - 16);
+          context.rect(x + offSet / 2, y + offSet / 2, w - offSet, h - offSet);
           context.stroke();
         }
       }
