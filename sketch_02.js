d2fdba29a7537645c482ab6279621b5706517e54
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-10-28 19:05:17 -0400

    set up initial black square

--- sketch_02.js
+++ sketch_02.js
@@ -1,13 +1,24 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = "black";
+
+    const x = 0;
+    const y = 0;
+    const w = width * 0.3;
+    const h = height * 0.3;
+
+    context.beginPath();
+    context.rect(x, y, w, h);
+    context.fill();
   };
 };
 
