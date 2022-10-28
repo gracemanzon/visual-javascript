bc7bef8475f85bcc9b5bc8b71da83a5c213a5d3c
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-10-28 19:40:48 -0400

    write a function to convert degrees to radians

--- sketch_02.js
+++ sketch_02.js
@@ -4,6 +4,11 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+const degToRad = (degrees) => {
+  return (degrees / 180) * Math.PI;
+};
+// ^ function to convert degrees to radians
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
@@ -13,25 +18,21 @@ const sketch = () => {
 
     const x = width * 0.5;
     const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const w = width * 0.01;
+    const h = height * 0.1;
 
     // square
     context.save();
     context.translate(x, y);
-    context.rotate(0.3);
+    // context.rotate(0.3);
+    // ^ computed in radians not degrees
+    context.rotate(degToRad(45));
+    // ^ rotating by degree after conversion in function degToRad
 
     context.beginPath();
     context.rect(-w * 0.5, -h * 0.5, w, h);
     context.fill();
     context.restore();
-
-    // circle - demoing how translate is culmulative and needs a save and restore to localize them
-    context.translate(100, 400);
-
-    context.beginPath();
-    context.arc(0, 0, 50, 0, Math.PI * 2);
-    context.fill();
   };
 };
 
