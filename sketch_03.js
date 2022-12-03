eb57062f70c454440f3148e70d1600e23243881a
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-12-03 13:21:23 -0500

    references comments

--- sketch_03.js
+++ sketch_03.js
@@ -5,6 +5,15 @@ const settings = {
   dimensions: [1080, 1080],
   animate: true,
 };
+// ^ animate: true is a function (bts function below)
+
+// const animate = () => {
+//   console.log("domestika");
+//   requestAnimationFrame(animate);
+//   // ^ browser window function with a parameter that to call on every frame (in this case the animate function)
+// };
+// animate();
+// // ^ animate does something (console.logs domestika) then requests the next frame, which triggers the function again and so on and so over essentially creating an animation loop
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -44,6 +53,7 @@ class Agent {
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
     this.radius = random.range(4, 12);
   }
+  // ^ give velocity property with x + y values (in this case they are random ranges to create different velocities for each instance of agent)
 
   bounce(width, height) {
     if (this.pos.x <= 0 || this.pos.x >= width) {
@@ -53,6 +63,7 @@ class Agent {
       this.vel.y *= -1;
     }
   }
+  // ^ keep vectors "inside" the canvas boundaries
 
   update() {
     this.pos.x += this.vel.x;
