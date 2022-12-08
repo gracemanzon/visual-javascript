4bb3c24afe8c4ad714defcbea1c1a995ad642a7b
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-12-07 22:26:00 -0500

    define proportions of the grid

--- sketch_04.js
+++ sketch_04.js
@@ -8,6 +8,23 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    const columns = 4;
+    const rows = 3;
+    const cells = columns * rows;
+    // ^ define grid specs
+
+    const gridWidth = width * 0.8;
+    const gridHeight = height * 0.8;
+    // ^ define grid boundaries
+
+    const cellWidth = width / columns;
+    const cellHeight = height / rows;
+    // ^ define cell specs
+
+    const marginX = (width - gridWidth) * 0.5;
+    const marginY = (height - gridHeight) * 0.5;
+    // ^ define margins - difference between grid boundaries and canvas
   };
 };
 
